a3c1e083823088db500679459b5a648099c5b663
Stanislav Yanakiev <stan5050@gmail.com>
2023-06-09 22:39:40 +0300

    'daily resolved tasks update'

---
+++ www.leetcode.com-solutions-master/main.py
@@ -0,0 +1,22 @@
+# Sort a list without a built in function
+my_list = [4, 8, 5, 2]
+sorted_list = []
+
+current_min = 0
+current_index = 0
+
+while my_list:
+    max_value = max(my_list)
+    min_value = 0
+    current_index = 0
+    for x in range(len(my_list)):
+        a = my_list[x]
+        if my_list[x] < max_value:
+            max_value = my_list[x]
+            current_index = x
+    min_value = max_value
+
+    sorted_list.append(min_value)
+    my_list.pop(current_index)
+
+print(sorted_list)
